b6fe94f349d72b6b7c6c06277de699799f1d57de
JoowonSuh <sjw2023@gmail.com>
2024-08-12 10:41:42 +0900

    initialize

---
+++ src/main/java/com/wsapoa/WsaPoaApplication.java
@@ -0,0 +1,13 @@
+package com.wsapoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsaPoaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WsaPoaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wsapoa/WsaPoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wsapoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsaPoaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
