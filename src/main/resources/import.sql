3f06cbec3553acd664825dfee8e937287731d64d
JoowonSuh <sjw2023@gmail.com>
2025-01-21 15:10:41 +0900

    mod: fix typo

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -9,19 +9,19 @@ VALUES (1,67431541296,
         2383,
         12031,
         'TEST',
-        true,
+        1,
         2352),
-       (2,33126117000, 2390, 5898, 'DRY CONTAINER - 20 FEET',true, 2350),
-       (3,67577728000, 2390, 12032, 'DRY CONTAINER - 40 FEET', true,2350),
-       (4,76201664000, 2695, 12032, 'DRY CONTAINER - 40 FEET HIGH CUBE', true,2350),
-       (5,85990371264, 2697, 13556, 'DRY CONTAINER - 45 FEET',true, 2352);
+       (2,33126117000, 2390, 5898, 'DRY CONTAINER - 20 FEET',1, 2350),
+       (3,67577728000, 2390, 12032, 'DRY CONTAINER - 40 FEET', 1,2350),
+       (4,76201664000, 2695, 12032, 'DRY CONTAINER - 40 FEET HIGH CUBE', 1,2350),
+       (5,85990371264, 2697, 13556, 'DRY CONTAINER - 45 FEET',1, 2352);
 INSERT INTO wsa_poa.products (used, height, length, product_id, product_volume, width, name)
-VALUES (true, 200, 400, 1, 24000000, 300, 'test');
+VALUES (1, 200, 400, 1, 24000000, 300, 'test');
 
 INSERT INTO wsa_poa.patterns (used, height, length, pattern_id, pattern_volume, width, name)
-VALUES (true, 2352, 1, 1, 1, 1, '1');
+VALUES (1, 2352, 1, 1, 1, 1, '1');
 
 INSERT INTO wsa_poa.pallets (used, height, length, pallet_id, width, name)
-VALUES (true, 150, 1200, 1, 1000, 'test'),
-       (true, 150, 1100, 2, 1100, 'test2'),
-       (true, 150, 1200, 3, 1100, 'test3');
+VALUES (1, 150, 1200, 1, 1000, 'test'),
+       (1, 150, 1100, 2, 1100, 'test2'),
+       (1, 150, 1200, 3, 1100, 'test3');
